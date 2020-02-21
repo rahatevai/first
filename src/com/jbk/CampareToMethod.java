fa07af3ed6d943d39626ad1dc512d6b1877d7c1f
rahatevai <61274297+rahatevai@users.noreply.github.com>
2020-02-21 12:23:07 +0530

    changes in sop

--- src/com/jbk/CampareToMethod.java
+++ src/com/jbk/CampareToMethod.java
@@ -9,6 +9,7 @@ public class CampareToMethod {
 		System.out.println(s1.compareTo(s2));
 		System.out.println(s1.compareTo(s3));
 		System.out.println(s1.compareTo(s4));
+		System.out.println(s1.compareTo(s2));
 	}
 
 }
